d23866ca90c4374b44f47835085bc769f69fd4c6
Rabbi Khan <Krabby2244@gmail.com>
2026-09-14 14:40:09 +0600

    feat: add express json body parser middleware

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
