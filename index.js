d05c92d2f1b1e4d53ab5315bdc0d80b602614c4e
Rabbi Khan <Krabby2244@gmail.com>
2026-09-14 14:40:23 +0600

    feat: add CORS headers middleware

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  res.setHeader(
+    'Access-Control-Allow-Origin',
+    'https://rabbi-khan.vercel.app/'
+  );
+
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  next();
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
