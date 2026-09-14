5cdb86f5ecfa3ae6554ad8e89a964d971e08808b
Rabbi Khan <Krabby2244@gmail.com>
2026-09-14 14:40:17 +0600

    feat: add urlencoded body parser middleware

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
